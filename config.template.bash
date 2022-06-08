75bfc6314a6dbdb41bc060352d2676c34a9af954
Paul Stodghill <paul.stodghill@usda.gov>
2022-06-08 12:00:55 -0400

    config.template.bash: Added HOWTO_CONDA_CMD.

--- config.template.bash
+++ config.template.bash
@@ -2,6 +2,10 @@
 
 # ------------------------------------------------------------------------
 
+if [ -e ${HOME}/miniconda3/condabin/conda ] ; then
+    export HOWTO_CONDA_CMD=${HOME}/miniconda3/condabin/conda
+fi
+
 if [ -e /programs/docker/bin/docker1 ] ; then
     export HOWTO_DOCKER_CMD=/programs/docker/bin/docker1
 fi
